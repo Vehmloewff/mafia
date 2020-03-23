83edfd13b176fa65d3f7439c90c512e3f637fbed
Jackson Mooring <me@jacksonmooring.com>
2020-03-23 16:37:31 -0500

    Serve public without checking for api

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,36 @@
 import polka from 'polka';
+import fs from 'fs';
 const app = polka();
 const PORT = process.env.PORT || 3000;
+let filesNames: string[] = [];
 
-app.get('/', (_, res) => {
-	res.end('Hello World!');
+fs.readdir(__dirname + '/../public', (err, files) => {
+	if (err) console.log(err.message);
+	else {
+		files.forEach(file => {
+			filesNames.push(file);
+		});
+	}
+});
+
+app.get('/:file', (req, res) => {
+	let result = (file: string) => {
+		let fileName = filesNames.filter(v => v == file);
+		fs.readFile(__dirname + '/../public/' + fileName, 'utf-8', (err, data) => {
+			if (fileName.length == 0) {
+				res.statusCode = 404;
+				res.end('404 Not Found');
+			} else if (err) {
+				console.log(err);
+				res.statusCode = 500;
+				res.end('Internal Server Error');
+			} else {
+				res.statusCode = 200;
+				res.end(data);
+			}
+		});
+	};
+	result(req.params.file); // Error: this needs to be updated in `typings/polka.d.ts`
 });
 
 app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
