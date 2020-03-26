a28e682ccc4d4b7c115981f06f9729c32861b025
Elijah Mooring <ejsisgreat@gmail.com>
2020-03-26 14:37:14 -0500

    Create an interfaces file

---
+++ src/game/interfaces.ts
@@ -0,0 +1,10 @@
+export interface Settings {
+	revealAllies: boolean;
+	openVote: boolean;
+	maxEach: number;
+	numberVillagers: number;
+	incorperateJudges: boolean;
+	maxArrestsPerRound: number;
+	roundsPerCitezensArrest: number;
+	snorts: string[];
+}
