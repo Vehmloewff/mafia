a92cacaee0df282fde600e69b323039407c1776e
Elijah Mooring <ejsisgreat@gmail.com>
2020-03-28 08:08:22 -0500

    Create a socket client

---
+++ src/client/socket.ts
@@ -0,0 +1,47 @@
+import { self, error, messageListener } from './store';
+import { get } from 'svelte/store';
+import { stringify } from 'query-string';
+
+export type Listener = () => void;
+
+export default function createSocket(gameId: string) {
+	const $self = get(self);
+	const socket = new WebSocket(`ws://${location.host}/api/games/${gameId}${stringify($self)}`);
+
+	function send(key: string, params: any = null) {
+		socket.send(JSON.stringify({ key, params }));
+	}
+
+	return new Promise(resolve => {
+		socket.onopen = () => {
+			socket.onmessage = data => {
+				const message = JSON.parse((data as any) as string);
+
+				// Handle errors
+				if (message.key === 'error') {
+					if (message.params.code === 'INVALID_GAME_ID') console.log('invalid game id');
+					else error.set(message.params);
+				}
+
+				// Listen for user info
+				else if (message.key === 'citizens-arrests-left') {
+					self.update($self => {
+						$self.citizensArrestsLeft = Number(message.params);
+						return $self;
+					});
+				}
+
+				// Handle all other messages
+				get(messageListener)(message.key, message.params);
+			};
+
+			resolve({
+				send,
+			});
+		};
+		socket.onerror = err => {
+			console.error(err);
+			resolve();
+		};
+	});
+}
---
+++ src/client/store.ts
@@ -0,0 +1,28 @@
+import { writable } from 'svelte/store';
+import { SafeUser, StartUser, User } from '../game/users';
+import { Settings } from '../game/interfaces';
+import defaultSnorts from '../game/default-snorts';
+
+export const users = writable<SafeUser[]>([]);
+export const settings = writable<Settings>({
+	revealAllies: true,
+	openVote: true,
+	maxEach: 2,
+	numberVillagers: 1,
+	incorperateJudges: true,
+	maxArrestsPerRound: 3,
+	roundsPerCitizensArrest: 3,
+	snorts: Object.keys(defaultSnorts),
+});
+export const self = writable<User>({
+	id: null,
+	name: null,
+	gender: null,
+	isDead: false,
+	isOwner: null,
+	citizensArrestsLeft: 1,
+	role: null,
+});
+export const error = writable<{ message: string; code: string }>(null);
+export const roundNumber = writable(0);
+export const messageListener = writable((messgageKey: string, params: unknown) => {});
