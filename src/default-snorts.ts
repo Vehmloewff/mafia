3b792de0f8a293de98ace2fa2cb4e5d110bd0c25
Elijah Mooring <ejsisgreat@gmail.com>
2020-04-05 16:45:07 -0500

    Add some more snorts

--- src/default-snorts.ts
+++ src/default-snorts.ts
@@ -1,19 +1,31 @@
 export default {
-	doubleQuick: {
-		url: '/snores/double-quick.mp3',
-		name: 'Double Quick',
-	},
 	oldSchool: {
 		url: '/snores/old-school.mp3',
 		name: 'Old School',
 	},
-	deepBreather: {
-		url: '/snores/deep-breather.mp3',
-		name: 'Deep Breather',
+	shortAndUgly: {
+		url: '/snores/short-and-ugly.wav',
+		name: 'Short And Ugly',
 	},
 	snort: {
 		url: '/snores/snort.mp3',
 		name: 'Snort',
+	},
+	busyPig: {
+		url: '/snores/busy-pig.wav',
+		name: 'Busy Pig',
+	},
+	doubleQuick: {
+		url: '/snores/double-quick.mp3',
+		name: 'Double Quick',
+	},
+	grumpyHunter: {
+		url: '/snores/grumpy-hunter.wav',
+		name: 'Grumpy Hunter',
+	},
+	deepBreather: {
+		url: '/snores/deep-breather.mp3',
+		name: 'Deep Breather',
 	},
 	// snarl: {
 	// 	url: '/snores/snarl.mp3',
